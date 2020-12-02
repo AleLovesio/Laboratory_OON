63a2e4de67deeb8d1f7d41f5e51f0d0f9e5ad0c5
AleLovesio <alessandrolovesio@gmail.com>
2020-12-02 16:35:54 +0100

    Archiving Lab6 files (Lab7 with slides numbering)

---
+++ Lab6/core/parameters.py
@@ -0,0 +1,6 @@
+import scipy.constants as const
+
+
+c = const.speed_of_light
+NUMBER_OF_CHANNELS = 10
+side_channel_occupancy = True
---
+++ Lab6/tasks/lab3.py
@@ -0,0 +1,34 @@
+from core import elements as elem
+from core import parameters as param
+from core import science_utils as sci_util
+from core import utils as util
+
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+if __name__ == "__main__":
+    root = Path(__file__).parent.parent
+    file = root / "resources" / "nodes.json"
+    network = elem.Network(file)
+    network_nodes_list = list(network.nodes.keys())
+    connections_list = []
+    for i in range(100):
+        [start_node, end_node] = util.sample_2_nodes(network_nodes_list)
+        connections_list.append(elem.Connection(start_node, end_node, 1))
+    for analysis_type in ["Latency", "SNR"]:
+        connections_list = network.stream(connections_list, analysis_type)
+        streams_snr_list = []
+        streams_latency_list = []
+        for connection in connections_list:
+            streams_snr_list.append(connection.snr)
+            streams_latency_list.append(connection.latency)
+        plt.figure()
+        plt.hist(streams_snr_list, bins=15)
+        if analysis_type == "Latency":
+            unit = "[s]"
+        else:
+            unit = "[dB]"
+        plt.xlabel(analysis_type+" Range "+unit)
+        plt.ylabel("Paths")
+        plt.title("Path choice: "+analysis_type)
+    plt.show()
